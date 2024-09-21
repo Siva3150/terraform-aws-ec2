8487a5cfedc2fc6b88fabb89d3778acf6e4b3a79
siva3150 <siva123@gmail.com>
2024-09-21 16:22:26 +0530

    terraform-aws-ec2

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-0e12349e784c6ec0a"]
+    tags = {
+        Name = "terraform"
+    }
+}
